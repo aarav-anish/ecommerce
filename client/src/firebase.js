be10d31671bc257a462365115ca195024e4c0293
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2024-05-20 01:13:45 +0530

    firebase setup and add register form

---
+++ client/src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = () => {};
+
+  const registerForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label for="email">Enter your email</label>
+        <input
+          type="email"
+          class="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoFocus
+        />
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </form>
+    );
+  };
+
   return (
-    <div>
-      <p>Register page</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register page</h4>
+          {registerForm()}
+        </div>
+      </div>
     </div>
   );
 };
