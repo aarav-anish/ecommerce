47efd6ccb7dc48c5138c1205171d954af5b4fc3c
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2024-06-01 01:39:57 +0530

    add conditional rendering

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
@@ -14,61 +14,13 @@ import {
   LogoutOutlined,
 } from "@ant-design/icons";
 
-const items1 = [
-  {
-    label: (
-      <Link to="/" class="text-decoration-none">
-        Home
-      </Link>
-    ),
-    key: "home",
-    icon: <AppstoreOutlined />,
-  },
-  {
-    label: "Username",
-    key: "SubMenu",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        label: "Dashboard",
-        key: "dashboard",
-      },
-      {
-        label: "Logout",
-        key: "logout",
-        icon: <LogoutOutlined />,
-      },
-    ],
-  },
-];
-
-const items2 = [
-  {
-    label: (
-      <Link to="/login" class="text-decoration-none">
-        Login
-      </Link>
-    ),
-    key: "login",
-    icon: <UserOutlined />,
-  },
-  {
-    label: (
-      <Link to="/register" class="text-decoration-none">
-        Register
-      </Link>
-    ),
-    key: "register",
-    icon: <UsergroupAddOutlined />,
-  },
-];
-
 const Header = () => {
   const [current, setCurrent] = useState("home");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state }));
 
-  const logout = async () => {
+  const handleLogout = async () => {
     console.log("logout is clicked");
     await signOut(auth);
     dispatch({
@@ -81,17 +33,68 @@ const Header = () => {
   const onClick = (e) => {
     console.log("click", e);
     setCurrent(e.key);
-    if (e.key === "logout") {
-      logout();
-    }
   };
+
+  const homeItem = [
+    {
+      label: (
+        <Link to="/" class="text-decoration-none">
+          Home
+        </Link>
+      ),
+      key: "home",
+      icon: <AppstoreOutlined />,
+    },
+  ];
+
+  const loggedInItem = [
+    {
+      label: user?.email.split("@")[0],
+      key: "SubMenu",
+      icon: <SettingOutlined />,
+      children: [
+        {
+          label: "Dashboard",
+          key: "dashboard",
+        },
+        {
+          label: "Logout",
+          key: "logout",
+          icon: <LogoutOutlined />,
+          onClick: handleLogout,
+        },
+      ],
+    },
+  ];
+
+  const loggedOutItem = [
+    {
+      label: (
+        <Link to="/login" class="text-decoration-none">
+          Login
+        </Link>
+      ),
+      key: "login",
+      icon: <UserOutlined />,
+    },
+    {
+      label: (
+        <Link to="/register" class="text-decoration-none">
+          Register
+        </Link>
+      ),
+      key: "register",
+      icon: <UsergroupAddOutlined />,
+    },
+  ];
+
   return (
     <div className="d-flex">
       <Menu
         onClick={onClick}
         selectedKeys={[current]}
         mode="horizontal"
-        items={items1}
+        items={homeItem}
         style={{ flex: "1" }}
         className="d-flex justify-content-start"
       />
@@ -99,7 +102,7 @@ const Header = () => {
         onClick={onClick}
         selectedKeys={[current]}
         mode="horizontal"
-        items={items2}
+        items={user ? loggedInItem : loggedOutItem}
         style={{ flex: "1" }}
         className="d-flex justify-content-end"
       />
